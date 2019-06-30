8f64e3950ce0878f099dffb078d6c11787c10f5b
samrat.basu01 <samrat.basu01@BLRKEC287992L.ad.infosys.com>
2019-06-30 17:07:45 +0530

    Updated

--- src/main/java/com/org/infy/binary/service/ListDocumentHelper.java
+++ src/main/java/com/org/infy/binary/service/ListDocumentHelper.java
@@ -11,12 +11,9 @@ import com.org.infy.binary.model.DocumentList;
 
 public class ListDocumentHelper {
 	private final static Logger log = Logger.getLogger(ListDocumentHelper.class);
+	static List<DocumentList> dlList = new ArrayList<DocumentList>();
 	
-	
-	public static List<DocumentList> displayDirectoryContents(File fileDir) {
-		List<DocumentList> dlList = new ArrayList<DocumentList>();
-		DocumentList dl = new DocumentList();
-		
+	public static List<DocumentList> displayDirectoryContents(File fileDir) {	
 		try {
 			File[] files = fileDir.listFiles();
 			for (File file : files) {
@@ -25,12 +22,12 @@ public class ListDocumentHelper {
 					displayDirectoryContents(file);
 				} else {
 					log.debug("Absolute Path:" + file.getCanonicalPath() + " Size:"+file.length()+" file Path"+file.getPath());
+					DocumentList dl = new DocumentList();
 					dl.setFileName(file.getName());
 					dl.setUrl(file.getCanonicalPath());
 					dl.setSize(file.length());
 					dl.setId(file.getParent()+"_"+file.getName());
 					dlList.add(dl);
-					dl = new DocumentList();
 				}
 			}
 		} catch (IOException e) {
